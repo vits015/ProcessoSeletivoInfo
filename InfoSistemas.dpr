551cbcdee26617459c872a8ed2a52652f9200ba4
Vitor Bittencourt <50675405+vits015@users.noreply.github.com>
2021-11-19 10:54:23 -0300

    memoryleak removido

--- InfoSistemas.dpr
+++ InfoSistemas.dpr
@@ -15,5 +15,4 @@ begin
   Application.CreateForm(TfrmPrincipal, frmPrincipal);
   Application.CreateForm(TfrmEnvioEmail, frmEnvioEmail);
   Application.Run;
-  ReportMemoryLeaksOnShutdown:=true;
 end.
